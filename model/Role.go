9a85ff82877d2001f45244a1f361c3769b6d8e73
Ahil <ahilcicillin@gmail.com>
2026-07-06 14:18:23 +0530

    removed space

--- model/Role.go
+++ model/Role.go
@@ -7,11 +7,11 @@ import (
 )
 
 type Role struct {
-	ID          uint           `gorm:"primaryKey" json:"id"`
-	Name        string         `gorm:"unique;not null" json:"name"`
-	CreatedAt   time.Time      `json:"created_at"`
-	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"`
-	
-	Permissions []Permission   `gorm:"many2many:role_permissions" json:"permissions,omitempty"`
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Name      string         `gorm:"unique;not null" json:"name"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
+
+	Permissions []Permission `gorm:"many2many:role_permissions" json:"permissions,omitempty"`
 }
