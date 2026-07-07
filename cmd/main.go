a92b43e14578301a9ee391379cecf1ace102c833
Ahil <ahilcicillin@gmail.com>
2026-07-07 13:14:22 +0530

    added main file in cmd folder  and added other files in tmp folfer

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"backend_institutions/internal/database"
+	"backend_institutions/internal/model"
+	"backend_institutions/internal/routes"
+	"backend_institutions/internal/seeds"
+	"github.com/gofiber/fiber/v3"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Warning: Error loading .env file, relying on environment variables or defaults")
+	}
+
+	app := fiber.New()
+	database.Connect()
+	err = database.DB.AutoMigrate(
+		&model.Institutions{},
+		&model.Department{},
+		&model.Faculty{},
+		&model.Student{},
+		&model.Fees{},
+		&model.User{},
+		&model.Role{},
+		&model.Permission{},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	seeds.RunSeeders()
+
+	routes.SetUpRoutes(app)
+
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8090"
+	}
+	log.Printf("Server starting on :%s", port)
+
+	log.Fatal(app.Listen(":" + port))
+}
